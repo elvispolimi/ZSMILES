3c1abad7180fb1198552cab7d13efcfe13acb6fd
gaccordi <gianmarco.accordi@polimi.it>
2023-11-21 11:16:12 +0000

    Fix openmp

--- libsmiles/include/cuda/compressor.cuh
+++ libsmiles/include/cuda/compressor.cuh
@@ -9,20 +9,20 @@
 
 #define WARP_SIZE  32
 #define BLOCK_SIZE WARP_SIZE
-#define GRID_SIZE  512
+#define GRID_SIZE  2056
 
 #define MAX_SMILES_LEN   96
-#define SMILES_PER_BLOCK 128
+#define SMILES_PER_BLOCK 1024
 
-#define SMILES_PER_DEVICE GRID_SIZE* BLOCK_SIZE* SMILES_PER_BLOCK
-#define CHAR_PER_DEVICE   SMILES_PER_DEVICE* MAX_SMILES_LEN
+#define SMILES_PER_DEVICE (std::size_t)GRID_SIZE* BLOCK_SIZE* SMILES_PER_BLOCK
+#define CHAR_PER_DEVICE   (std::size_t)SMILES_PER_DEVICE* MAX_SMILES_LEN
 
 namespace smiles {
   namespace cuda {
     class base_compressor {
     public:
       using cost_type          = uint_fast16_t;
-      using index_type         = int;
+      using index_type         = size_t;
       using pattern_index_type = uint_fast8_t;
       using smiles_type        = std::string::value_type;
       // pre-allocate memory to avoid useless memory operations
