14fe078f1148347ccaa265a3c83eb1ebe60193e1
gaccordi <10587213@polimi.it>
2025-03-07 16:00:32 +0100

    Add likwid utils

--- libsmiles/include/zsmiles/likwid_utils.hpp
+++ libsmiles/include/zsmiles/likwid_utils.hpp
@@ -11,4 +11,13 @@
   #define LIKWID_MARKER_STOP(regionTag)
   #define LIKWID_MARKER_CLOSE
   #define LIKWID_MARKER_GET(regionTag, nevents, events, time, count)
+
+  #define NVMON_MARKER_INIT
+  #define NVMON_MARKER_THREADINIT
+  #define NVMON_MARKER_SWITCH
+  #define NVMON_MARKER_REGISTER(regionTag)
+  #define NVMON_MARKER_START(regionTag)
+  #define NVMON_MARKER_STOP(regionTag)
+  #define NVMON_MARKER_CLOSE
+  #define NVMON_MARKER_GET(regionTag, nevents, events, time, count)
 #endif
